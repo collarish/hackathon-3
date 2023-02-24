63058effad42c710b6adeba7ed14f93a6540b975
Rachel Young <reyoung255@gmail.com>
2023-02-24 15:20:40 +0100

    home page styles

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -1,19 +1,22 @@
+@include('common.html-start')
+
 @foreach ($animals_owners as $animals_owner)
-    <div class="dog-info">
+    <div class="animal-container">
         <img src="/images/pets/{{ $animals_owner->path}}">
-        {{ $animals_owner->name }}
-        {{ $animals_owner->breed }}
-        {{ $animals_owner->age }} years
-        {{ $animals_owner->weight }} kg
-        <a href="animals/{{$animals_owner->id}}">Edit dog</a>
-    </div>
-    <div class="owner-info"> 
-        Owner:
-        {{ $animals_owner->first_name . " " . $animals_owner->surname }} 
-        {{ $animals_owner->email }}
-        {{ $animals_owner->phone }}
-        {{ $animals_owner->address }}
-        <a href="owner/detail/{{$animals_owner->owner_id}}">Edit owner</a>
+        <div class="dog-info">
+            <div class="dog-name">{{ $animals_owner->name }}</div>
+            <div>{{ $animals_owner->breed }}</div>
+            <div>{{ $animals_owner->age }} years</div>
+            <div>{{ $animals_owner->weight }} kg</div>
+            <a href="animals/{{$animals_owner->id}}">Edit dog</a>
+        </div>
+        <div class="owner-info"> 
+            <div>Owner: {{ $animals_owner->first_name . " " . $animals_owner->surname }} </div>
+            <div>{{ $animals_owner->email }}</div>
+            <div>{{ $animals_owner->phone }}</div>
+            <div>{{ $animals_owner->address }}</div>
+            <a href="owner/detail/{{$animals_owner->owner_id}}">Edit owner</a>
+        </div>
     </div>
     <hr>
 @endforeach
@@ -31,4 +34,7 @@ Owners:
 @foreach ($owners as $owner)
     <li>{{ $owner->first_name }} </li> 
 @endforeach --}}
+
+@include('common.html-start')
+
 
