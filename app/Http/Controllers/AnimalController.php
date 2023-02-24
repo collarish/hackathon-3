40ba0a23edfd26b8ad37804de603cee4714dbce3
Alena Kellnerova <kellnerovaajka@gmail.com>
2023-02-24 11:07:24 +0100

    animal model

---
+++ app/Http/Controllers/AnimalController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AnimalController extends Controller
+{
+    public function index()
+    {
+        $animal = DB::select 
+    }
+
+
+    public function list()
+    {
+        $animals = DB::select()
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AnimalController;
+use App\Models\Animal;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/animals', [AnimalController::class, 'index'])->name('animals.animal');
